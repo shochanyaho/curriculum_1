64007f2294d95109cd862023917f2c69f3663dcb
shochanyaho <78130521+shochanyaho@users.noreply.github.com>
2021-02-03 22:09:55 +0900

    課題評価テスト
    Task1_4.javaの提出

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -22,17 +22,17 @@ public class Task1_4 {
          String name = "alice";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-         if (name==CONST_NAME && pass==CONST_PASS ) {
+         if (name.equals(CONST_NAME) && pass.equals(CONST_PASS)) {
              System.out.println(CONST_MSG_SUCCESS);
          } 
 
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-           else if (name==CONST_NAME) {
+           else if (name.equals(CONST_NAME)) {
                System.out.println(CONST_MSG_ERROR_PASS);
            }
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-             else if (pass==CONST_PASS) {
+             else if (pass.equals(CONST_PASS)) {
                  System.out.println(CONST_MSG_ERROR_NAME);
              }
 
